1fdf78e8d1fbd73baea18e9e7022be983cef3221
muji <muji@tmpfs.org>
2023-01-01 11:42:40 +0800

    Fix typo in error message.

--- ios/Classes/SwiftIcloudStoragePlugin.swift
+++ ios/Classes/SwiftIcloudStoragePlugin.swift
@@ -390,7 +390,7 @@ public class SwiftIcloudStoragePlugin: NSObject, FlutterPlugin {
   }
   
   let argumentError = FlutterError(code: "E_ARG", message: "Invalid Arguments", details: nil)
-  let containerError = FlutterError(code: "E_CTR", message: "Invalid containerId, or user is not signed in, or user disabled iCould permission", details: nil)
+  let containerError = FlutterError(code: "E_CTR", message: "Invalid containerId, or user is not signed in, or user disabled iCloud permission", details: nil)
   let fileNotFoundError = FlutterError(code: "E_FNF", message: "The file does not exist", details: nil)
   
   private func nativeCodeError(_ error: Error) -> FlutterError {
